e0db480de1be29581c47ec4ae8fafc5e83687dd8
myGit <1718282277@qq.com>
2019-12-22 09:26:06 +0800

    流2

--- src/Main2.java
+++ src/Main2.java
@@ -2,8 +2,5 @@ public class Main2 {
 
     public static void main(String [] args){
         System.out.println("hello git2222");
-        System.out.println("hello 3333");
-        System.out.println("hello 5555");
-		System.out.println("hello 6666");
     }
 }
