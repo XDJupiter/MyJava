93b5115d2d86f961f9de5e3dc9fd7a7f0fde9f16
myGit <1718282277@qq.com>
2019-12-22 09:32:18 +0800

    夹1-7

--- src/Main2.java
+++ src/Main2.java
@@ -1,6 +1,8 @@
 public class Main2 {
 
     public static void main(String [] args){
+        System.out.println("hello 111111");
         System.out.println("hello git2222");
+        System.out.println("hello 7777");
     }
 }
